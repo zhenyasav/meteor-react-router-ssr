00b760c2229ec08a4c59f9da2625211c4f71344f
eXon <trembl.ben@gmail.com>
2015-07-31 22:29:58 -0400

    Fixes for the URL/cookies/queries

--- lib/server.jsx
+++ lib/server.jsx
@@ -1,3 +1,30 @@
+// meteor algorithm to check if this is a meteor serving http request or not
+function IsAppUrl(req) {
+  var url = req.url
+  if(url === '/favicon.ico' || url === '/robots.txt') {
+    return false;
+  }
+
+  // NOTE: app.manifest is not a web standard like favicon.ico and
+  // robots.txt. It is a file name we have chosen to use for HTML5
+  // appcache URLs. It is included here to prevent using an appcache
+  // then removing it from poisoning an app permanently. Eventually,
+  // once we have server side routing, this won't be needed as
+  // unknown URLs with return a 404 automatically.
+  if(url === '/app.manifest') {
+    return false;
+  }
+
+  // Avoid serving app HTML for declared routes such as /sockjs/.
+  if(RoutePolicy.classify(url)) {
+    return false;
+  }
+
+  // we only need to support HTML pages only
+  // this is a check to do it
+  return /html/.test(req.headers['accept']);
+}
+
 ReactRouterSSR.Run = function(routes) {
   Meteor.publish('TestItems', function () {
     return TestItems.find();
@@ -7,15 +34,19 @@ ReactRouterSSR.Run = function(routes) {
   const Location = ReactRouter.lib.Location;
   const url = Npm.require('url');
   const Fiber = Npm.require('fibers');
+  const cookieParser = Npm.require('cookie-parser');
 
   Meteor.bindEnvironment(function() {
+    // Parse cookies for the login token
+    WebApp.rawConnectHandlers.use(cookieParser());
+
     WebApp.rawConnectHandlers.use(Meteor.bindEnvironment(function(req, res, next) {
-      if (req.url !== '/') {
+      if (!IsAppUrl(req)) {
         next();
         return;
       }
 
-      var parsedUrl = url.parse(req.url);
+      var parsedUrl = url.parse(req.url, true);
       var location = new Location(parsedUrl.pathname, parsedUrl.query);
 
       Router.run(routes, location, function(error, initialState, transition) {
@@ -58,9 +89,9 @@ ReactRouterSSR.Run = function(routes) {
 
           originalWrite.call(this, data);
         };
-      });
 
-      next();
+        next();
+      });
     }));
   })();
 };
--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 Package.describe({
   name: 'reactrouter:react-router-ssr',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'server-side rendering for react-router',
   git: 'https://github.com/thereactivestack/meteor-react-router-ssr.git',
   documentation: 'README.md'
+});
+
+Npm.depends({
+  'cookie-parser': '1.3.5'
 });
 
 Package.onUse(function(api) {
@@ -12,6 +16,8 @@ Package.onUse(function(api) {
     'reactrouter:react-router@0.1.1',
     'meteorhacks:fast-render@2.7.1'
   ]);
+
+  api.use(['routepolicy'], ['server']);
 
   api.add_files(['lib/react-router-ssr.js']);
   api.add_files(['lib/client.jsx'], 'client');
