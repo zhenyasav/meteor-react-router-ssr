734042d4af102e60b307ce8d12b69f6b318ce8a7
eXon <trembl.ben@gmail.com>
2015-09-11 21:06:12 -0400

    Publish 1.9

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'reactrouter:react-router-ssr',
   version: '0.1.9',
-  summary: 'Server-side rendering for react-router and react-meteor-data rehydratation using Meteor subscriptions.',
+  summary: 'Server-side rendering for react-router and react-meteor-data rehydratating Meteor subscriptions',
   git: 'https://github.com/thereactivestack/meteor-react-router-ssr.git',
   documentation: 'README.md'
 });
