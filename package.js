a4be5dc07f2b86c1d034052c2e5c25b5ffc181d3
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-23 19:14:30 -0400

    Release 0.2.13

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'reactrouter:react-router-ssr',
-  version: '0.2.12',
+  version: '0.2.13',
   summary: 'Server-side rendering for react-router and react-meteor-data rehydratating Meteor subscriptions',
   git: 'https://github.com/thereactivestack/meteor-react-router-ssr.git',
   documentation: 'README.md'
