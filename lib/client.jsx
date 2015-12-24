b386bd6c64b6dbd0576a2f50e78c54001b3333af
Dino Scheidt <scheidtrd+github.com@gmail.com>
2015-12-24 05:52:04 +0100

    Client attaches rootElementType & rootElementAttributes
    Adding rootElementType & rootElementAttributes when a new element is created.

--- lib/client.jsx
+++ lib/client.jsx
@@ -9,12 +9,25 @@ ReactRouterSSR.Run = function(routes, clientOptions) {
 
   Meteor.startup(function() {
     const rootElementName = clientOptions.rootElement || 'react-app';
+    const rootElementType = clientOptions.rootElementType || 'div';
+    const attributes = clientOptions.rootElementAttributes instanceof Array ? clientOptions.rootElementAttributes : [];
     let rootElement = document.getElementById(rootElementName);
 
     // In case the root element doesn't exist, let's create it
     if (!rootElement) {
-      rootElement = document.createElement('div');
+      rootElement = document.createElement(rootElementType);
       rootElement.id = rootElementName;
+
+      // check if a 2-dimensional array was passed... if not, be nice and handle it anyway
+      if(attributes[0] instanceof Array) {
+        // set attributes
+        for(var i = 0; i < attributes.length; i++) {
+          rootElement.setAttribute(attributes[i][0], attributes[i][1]);
+        }
+      } else if (attributes.length > 0){
+        rootElement.setAttribute(attributes[0], attributes[1]);
+      }
+
       document.body.appendChild(rootElement);
     }
 
