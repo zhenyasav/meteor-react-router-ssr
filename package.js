2d0d2e4f71142d51ac93867f7a0d1a304ef97372
Dino Scheidt <github@dino-scheidt.de>
2015-12-29 07:40:46 +0100

    Add mongo instances
    Inits mongo collections by making them instance aware, required for SSR
    of non ReactMeteorData

--- package.js
+++ package.js
@@ -29,6 +29,8 @@ Package.onUse(function(api) {
 
   api.use(['routepolicy@1.0.5'], ['server']);
 
+  api.use(['dburles:mongo-collection-instances'], ['server']);
+
   api.add_files(['lib/react-router-ssr.js']);
 
   api.add_files(['lib/client.jsx'], 'client');
